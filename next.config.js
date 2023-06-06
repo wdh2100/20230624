607d09fc12a43829326604a95d37639aac8822ec
Woo Dohyeong <wdh2100@gmail.com>
2023-06-07 00:25:20 +0900

    feat: add next-export-optimize-images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+const withExportImages = require('next-export-optimize-images')
+
+
+module.exports = withExportImages({
+    output: 'export',
+    assetPrefix: isProd ? 'https://wdh2100.github.io/20230624/' : '',
+})
