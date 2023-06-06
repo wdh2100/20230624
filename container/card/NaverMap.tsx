c1d98d2fc9a46ef2255b355ee5e37c6ee9f3cfdf
Woo Dohyeong <wdh2100@gmail.com>
2023-06-06 23:54:25 +0900

    Fix: naver map

--- container/card/NaverMap.tsx
+++ container/card/NaverMap.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Script from 'next/script';
-import useMap from "../../hooks/useMap";
 import styles from "@/app/page.module.css";
 
 const CLIENT_ID = 'lhwi4yf1te'
 
 
 const NaverMap = () => {
-    useMap();
+    useEffect(() => {
+        const birthDayPartyLocation = new naver.maps.LatLng(37.1064982, 127.1918814),
+            map = new naver.maps.Map('map', {
+                center: birthDayPartyLocation.destinationPoint(0, 500),
+                zoom: 15
+            }),
+            marker = new naver.maps.Marker({
+                map: map,
+                position: birthDayPartyLocation
+            });
+
+        const contentString = [
+            '<div style="color: black">',
+            '   <h3>송담고택</h3>',
+            '   <p>경기 용인시 처인구 이동읍 어진로 780<br />',
+            '</div>'
+        ].join('');
+
+        const infowindow = new naver.maps.InfoWindow({
+            content: contentString
+        });
+
+        infowindow.open(map, marker);
+    }, []);
 
     return (
         <div style={{paddingTop: '1rem'}}>
