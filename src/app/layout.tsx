2fc5f867ce9f5ccfa47a51891fbc433e9554f2f9
Woo Dohyeong <wdh2100@gmail.com>
2023-06-07 23:26:48 +0900

    fix: Add og url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,8 @@ export const metadata: Metadata = {
     openGraph: {
         title: '우윤슬 돌잔치',
         description: '우윤슬 돌잔치(20230624)',
-        images: 'https://github.com/wdh2100/20230624/blob/main/public/photo/small/230526%EC%9A%B0%EC%9C%A4%EC%8A%AC-320.jpg'
+        images: 'https://github.com/wdh2100/20230624/blob/main/public/photo/small/230526%EC%9A%B0%EC%9C%A4%EC%8A%AC-320.jpg',
+        url: 'https://github.com/wdh2100/20230624',
     },
     icons: {
         icon: `https://github.com/wdh2100/20230624/blob/main/src/app/favicon.ico`,
