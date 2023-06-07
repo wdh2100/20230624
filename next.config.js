3ac4bf237de3da3713cf7dde3680d9deeac7b8cd
Woo Dohyeong <wdh2100@gmail.com>
2023-06-07 22:49:08 +0900

    feat: off image optimize

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
-const withExportImages = require('next-export-optimize-images')
-
-
-module.exports = withExportImages({
+module.exports = {
+    reactStrictMode: true,
     output: 'export',
     assetPrefix: isProd ? 'https://wdh2100.github.io/20230624/' : '',
-})
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+    },
+}
