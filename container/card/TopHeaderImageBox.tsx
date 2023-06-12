1bb4d1c96e8ed62a2a4820aad96c49dc634f5cb6
Woo Dohyeong <wdh2100@gmail.com>
2023-06-12 22:04:56 +0900

    fix(gif): Change main image

--- container/card/TopHeaderImageBox.tsx
+++ container/card/TopHeaderImageBox.tsx
@@ -10,7 +10,7 @@ export const prefix =
 const TopHeaderImageBox = () => {
     return <Box component="span" sx={{p: 2}}>
         <div style={{width: '100%', height: '500px', position: 'relative'}}>
-            <Image src={`${prefix}/photo/medium/230526우윤슬-071.jpg`} fill priority
+            <Image src={`${prefix}/photo/main/card-image.gif`} fill priority
                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={'main'} quality={100}/>
         </div>
     </Box>
