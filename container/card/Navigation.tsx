1fa770ffddb0be0818c6094056f10342b988cb4b
Woo Dohyeong <wdh2100@gmail.com>
2023-06-12 22:05:25 +0900

    feat: Implement auto remove navigation

--- container/card/Navigation.tsx
+++ container/card/Navigation.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import VerticalAlignTopIcon from '@mui/icons-material/VerticalAlignTop';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Paper from '@mui/material/Paper';
-import {useScrollYPosition} from "../../hooks/useScrollPosition";
+import {useScrollPosition} from "../../hooks/useScrollPosition";
 import {Slide} from "@mui/material";
 
 const Navigation = () => {
-    const [value, setValue] = React.useState(0);
-    const yPosition = useScrollYPosition();
+    const [value, setValue] = useState(0);
+    const [firstRender, setFirstRender] = useState(true);
+    const {y = 0} = useScrollPosition();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFirstRender(false);
+        }, 2000);
+
+        return () => {
+            setFirstRender(true);
+            clearTimeout(timer);
+        };
+    }, [y])
 
     return (
-        <Slide direction="up" in={yPosition > 10}>
+        <Slide direction="up" in={firstRender && y > 10}>
             <Paper sx={{
                 position: 'fixed',
                 bottom: 0,
@@ -33,7 +46,7 @@ const Navigation = () => {
                         window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
                     }}/>
                     <BottomNavigationAction label="Photo" icon={<ArchiveIcon/>} onClick={() => {
-                        const top = Number(document?.getElementById('photo')?.offsetTop) - 50 ?? 0;
+                        const top = Number(document?.getElementById('photo')?.offsetTop) ?? 0;
                         console.log(top);
                         window.scrollTo({top, left: 0, behavior: 'smooth'})
                     }}/>
