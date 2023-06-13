a5014eb3a8819d939a9ad5c753fb30691d895b03
Woo Dohyeong <wdh2100@gmail.com>
2023-06-13 15:12:32 +0900

    fix: Remove top message

--- container/card/TopHeaderImageBox.tsx
+++ container/card/TopHeaderImageBox.tsx
@@ -9,24 +9,22 @@ export const prefix =
         : "";
 
 const TopHeaderImageBox = () => {
-    return <div style={{height: '100vh'}}>
-        <h1 style={{
-            textAlign: 'center',
-            padding: '1rem'
-        }}>🎉우윤슬 돌잔치🎉</h1>
-        <Box component="span" sx={{p: 2}}>
-            <div style={{width: '100%', height: '70vh', position: 'relative'}}>
-                <Image src={`${prefix}/photo/main/card-image.gif`} fill priority
-                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={'main'} quality={100}/>
-            </div>
-        </Box>
-        <h3 style={{textAlign: "center"}}>
-            <b style={{color: 'green'}}>아빠</b> 우도형 <Link href={'https://20191116.github.io/'}>❤️</Link> <b
-            style={{color: 'pink'}}>엄마</b> 윤이경
-        </h3>
-        <h2 style={{textAlign: "center"}}>
-            2023년 6월 24일 토요일 오후 12시<br/>송담고택</h2>
-    </div>
+    return (
+        <div style={{height: '100vh'}}>
+            <Box component="span" sx={{p: 2}}>
+                <div style={{width: '100%', height: '70vh', position: 'relative'}}>
+                    <Image src={`${prefix}/photo/main/card-image.gif`} fill priority
+                           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt={'main'} quality={100}/>
+                </div>
+            </Box>
+            <h3 style={{textAlign: "center"}}>
+                <b style={{color: 'green'}}>아빠</b> 우도형 <Link href={'https://20191116.github.io/'}>❤️</Link> <b
+                style={{color: 'pink'}}>엄마</b> 윤이경
+            </h3>
+            <h2 style={{textAlign: "center"}}>
+                2023년 6월 24일 토요일 오후 12시<br/>송담고택</h2>
+        </div>
+    )
 }
 
 
