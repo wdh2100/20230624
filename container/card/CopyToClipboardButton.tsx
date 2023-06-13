33e311a4d93e0e4891ec2597b42fa29bd99ee35c
Woo Dohyeong <wdh2100@gmail.com>
2023-06-13 09:28:21 +0900

    feat: Add react-copy-to-clipboard

--- container/card/CopyToClipboardButton.tsx
+++ container/card/CopyToClipboardButton.tsx
@@ -1,19 +1,21 @@
 import {useState} from 'react'
 import {IconButton, Snackbar} from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const CopyToClipboardButton = () => {
     const [open, setOpen] = useState(false)
     const handleClick = () => {
         setOpen(true)
-        navigator.clipboard.writeText('경기 용인시 처인구 이동읍 어진로 780').then();
     }
 
     return (
         <>
-            <IconButton onClick={handleClick} color="primary">
-                <ShareIcon/>
-            </IconButton>
+            <CopyToClipboard onCopy={handleClick} text={'경기 용인시 처인구 이동읍 어진로 780'}>
+                <IconButton onClick={handleClick} color="primary">
+                    <ShareIcon/>
+                </IconButton>
+            </CopyToClipboard>
             <Snackbar
                 open={open}
                 onClose={() => setOpen(false)}
